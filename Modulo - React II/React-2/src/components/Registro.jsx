07d8900af7b76e523de15b9544a87291f18bb329
tiMeCL <dbmarsot@gmail.com>
2024-03-10 22:15:07 -0300

    Modificacion de archivo registro.jsx y creacion de Style.css dentro de carpeta components

--- Modulo - React II/React-2/src/components/Registro.jsx
+++ Modulo - React II/React-2/src/components/Registro.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import Formulario from './Formulario'
 import SocialButton from './SocialButton'
 import Alert from './Alert'
-
+import  './Style.css'
 
 
 export default function Registro() {
@@ -17,10 +17,12 @@ export default function Registro() {
     
   
     return (
-    <div>
+    <div className='Carta'>
+      <div className='Redes'>
       <SocialButton icon="fab fa-facebook"/>
       <SocialButton icon="fab fa-twitter" />
       <SocialButton icon="fab fa-google" />
+      </div>
       <Formulario handleRegister={handleAlert} />
       {alertMessage && <Alert message={alertMessage} type={alertType} />}
       
